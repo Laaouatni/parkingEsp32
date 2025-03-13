d6c24e18bbc33a154bea8a585795b4f668f80b9d
Laaouatni <laaouatni.anas@outlook.it>
2025-03-13 22:30:01 +0100

    save

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <WiFi.h>
 
 AsyncWebSocket ws("/ws");
+String         completeWsString = "";
 
 void setup() {
   Serial.begin(115200);
@@ -17,31 +18,41 @@ void setup() {
                 AsyncWebSocketClient *client,
                 AwsEventType type, void *arg, uint8_t *data,
                 size_t len) {
-    const bool isConnecting    = type == WS_EVT_CONNECT;
-    const bool isDisconnecting = type == WS_EVT_DISCONNECT;
-    const bool hasData         = type == WS_EVT_DATA;
-
-    if (isConnecting) {
-      Serial.println(
-        "WebSocket Client Connected! IP Address: " +
-        client->remoteIP().toString());
-    }
-    if (isDisconnecting) {
-      Serial.println(
-        "WebSocket Client Disconnected! IP Address: " +
-        client->remoteIP().toString());
-    }
-
+    const bool hasData = type == WS_EVT_DATA;
     if (!hasData) { return; }
-
-    const String thisStringData =
+    
+    const String thisWsReceivedStringData =
       String((char *) data).substring(0, len);
 
-    ws.textAll(thisStringData);
-    Serial2.println(thisStringData);
-    Serial.println("⬅️ ho ricevuto dal CLIENT un messaggio "
-                   "e ➡️ inoltrato ai altri: " +
-                   String(thisStringData));
+    Serial.println(thisWsReceivedStringData);
+
+    //   const bool isConnecting    = type ==
+    //   WS_EVT_CONNECT; const bool isDisconnecting = type
+    //   == WS_EVT_DISCONNECT; const bool hasData         =
+    //   type == WS_EVT_DATA;
+
+    //   if (isConnecting) {
+    //     Serial.println(
+    //       "WebSocket Client Connected! IP Address: " +
+    //       client->remoteIP().toString());
+    //   }
+    //   if (isDisconnecting) {
+    //     Serial.println(
+    //       "WebSocket Client Disconnected! IP Address: " +
+    //       client->remoteIP().toString());
+    //   }
+
+    //   if (!hasData) { return; }
+
+    //   const String thisStringData =
+    //     String((char *) data).substring(0, len);
+
+    //   ws.textAll(thisStringData);
+    //   Serial2.println(thisStringData);
+    //   Serial.println("⬅️ ho ricevuto dal CLIENT un
+    //   messaggio "
+    //                  "e ➡️ inoltrato ai altri: " +
+    //                  String(thisStringData));
   });
 }
 
