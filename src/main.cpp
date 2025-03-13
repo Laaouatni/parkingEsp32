2f37038900552a3c845186a85f96afdeb13cf097
Laaouatni <laaouatni.anas@outlook.it>
2025-03-13 22:11:48 +0100

    save

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,14 @@
 
 AsyncWebSocket ws("/ws");
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  WiFi.begin("nomeWifi", "passwordWifi");
+  while (WiFi.status() != WL_CONNECTED) {};
+  Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
